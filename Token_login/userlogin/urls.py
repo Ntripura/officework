410ba2f40d59c13a9159553b958fb85dfe370dc3
Ntripura <tripurasuresh10@gmail.com>
2024-08-29 16:09:19 +0530

    reminder, dummy, relarionship

--- Token_login/userlogin/urls.py
+++ Token_login/userlogin/urls.py
@@ -2,14 +2,18 @@ from django.urls import path
 from userlogin import views
 from userlogin import models
 
+
 urlpatterns = [
     path('signup/', views.RegisterUser.as_view()),
     path('login/', views.LoginUser.as_view()),
     #path('userdetails/',views.UserDetails.as_view())
     path('userdetails/<str:pk>/', views.UserDetails.as_view()),
+    path('userdetails/',views.UserDetails.as_view()),
     path('dummycreate/',views.DummyRelationDetails.as_view()),  #post
-    path('dummyget/',views.DummyRelationDetails.as_view())  #get
-   # path('edit/<str:pk>/', views.Employee.as_view()),
-    # path('editpatch/<str:pk>/', views.Employee.as_view()),
-    # path('delete/<str:pk>/', views.Employee.as_view()),
+    path('dummyget/',views.DummyRelationDetails.as_view()),  #get
+    path('remindercreate/',views.ReminderDetails.as_view()),  #post
+    path('reminders/',views.ReminderDetails.as_view()),  #get
+    path('pcreate/',views.PDetails.as_view()),  #post
+    path('bcreate/',views.BDetails.as_view()),  #post
+
 ]
--- Token_login/userlogin/views.py
+++ Token_login/userlogin/views.py
@@ -15,6 +15,7 @@ from Token_login.auth import authenticate, validate_payload
 from django.contrib.auth.hashers import make_password, check_password
 import json
 import jwt
+import os
 
 from bson import ObjectId
 
@@ -169,3 +170,143 @@ class DummyRelationDetails(View):
             print(context)
             return JsonResponse(context)   
     
+    
+class ReminderDetails(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(ReminderDetails, self).dispatch(request, *args, **kwargs)
+    
+    @authenticate
+    def post(self, request):
+        data = json.loads(request.body.decode('utf-8'))
+        form = forms.ReminderForm(data)
+        print("From reminder post",request.user)
+        print(request.user['id'])
+        if form.is_valid():
+            message = form.cleaned_data['message']
+            target = form.cleaned_data['target']
+            now = datetime.now(timezone.utc)
+            time =now.strftime("%Y-%m-%d %H:%M:%S")
+            if target <= time:
+                raise forms.ValidationError("Target must be later than today date")
+                return super(ReminderForm, self).clean()
+            consumer_id = request.user['id']
+            con = models.ReminderModel(consumer_id = consumer_id,created = time,
+                                            message = message, target = target)
+            con.save()                               
+            #form.save()
+            return JsonResponse({'error':'false', 'msg':'Reminder created'})  
+        else:
+            return JsonResponse({'error':'true','msg':'Reminder creation failed','form':form.errors})
+        
+    
+    @authenticate
+    def get(self,request,pk=None):
+       form = forms.ReminderForm()
+       if pk is not None:
+            dataget = models.ReminderModel.objects.get(pk=ObjectId(pk))
+            context = {'consumer':dataget.consumer, 'created': dataget.created,
+                       'message':dataget.message,'target': dataget.target}
+            print(context)
+            return JsonResponse(context)   
+       else:
+            data = models.ReminderModel.objects.all()
+            print(data)
+            context = {'reminders': list(data.values('consumer','created',
+                                                     'message','target',))}
+            print(context)
+            return JsonResponse(context)   
+    
+    
+    
+class PDetails(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(PDetails, self).dispatch(request, *args, **kwargs)
+    
+    @authenticate
+    def post(self, request):
+        data = json.loads(request.body.decode('utf-8'))
+        print("From business post",request.user)
+        print(request.user['id'])
+        if data['category'] == "prof":
+            form = forms.ProfessionalForm(data)
+            if form.is_valid():
+                uid = os.urandom(3).hex().upper()
+                name = form.cleaned_data['name']
+                category = form.cleaned_data['category']
+                subcategory = form.cleaned_data['subcategory']
+                country= form.cleaned_data['country']
+                contact_person =  form.cleaned_data['contact_person']
+                contact_phone = form.cleaned_data['contact_phone']
+                email = form.cleaned_data['email']
+                password = form.cleaned_data['password']
+                address= form.cleaned_data['address']
+                correspondance_address =  form.cleaned_data['correspondance_address']
+                alias_name = form.cleaned_data['alias_name']
+                legal_structure = form.cleaned_data['legal_structure']
+                company_website = form.cleaned_data['company_website']
+                registration_no= form.cleaned_data['registration_no']
+                now = datetime.now(timezone.utc)
+                time =now.strftime("%Y-%m-%d %H:%M:%S")
+                created = time
+                con = models.ProfessionalModel(uid = uid,name = name, category = category, 
+                                           subcategory = subcategory,country = country, 
+                            contact_person = contact_person,contact_phone = contact_phone,
+                            email = email,password = password, address = address,
+                            correspondance_address = correspondance_address,
+                            alias_name = alias_name,legal_structure = legal_structure,
+                            company_website = company_website, joined = created,
+                            registration_no = registration_no)
+                con.save()
+                return JsonResponse({'error':'false', 'msg':'Professional Data created'})  
+            else:
+                return JsonResponse({'error':'true','msg':'Professional creation failed','form':form.errors})     
+    
+        
+class BDetails(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(BDetails, self).dispatch(request, *args, **kwargs)
+    
+    @authenticate
+    def post(self, request):
+        data = json.loads(request.body.decode('utf-8'))
+        print("From business post",request.user)
+        print(request.user['id'])
+        if data['category'] == "business":
+            form = forms.BusinessForm(data)
+            if form.is_valid():
+                uid = os.urandom(3).hex().upper()
+                name = form.cleaned_data['name']
+                category = form.cleaned_data['category']
+                subcategory = form.cleaned_data['subcategory']
+                country= form.cleaned_data['country']
+                contact_person =  form.cleaned_data['contact_person']
+                contact_phone = form.cleaned_data['contact_phone']
+                email = form.cleaned_data['email']
+                password = form.cleaned_data['password']
+                address= form.cleaned_data['address']
+                correspondance_address =  form.cleaned_data['correspondance_address']
+                alias_name = form.cleaned_data['alias_name']
+                legal_structure = form.cleaned_data['legal_structure']
+                company_website = form.cleaned_data['company_website']
+                registration_no= form.cleaned_data['registration_no']
+                now = datetime.now(timezone.utc)
+                time =now.strftime("%Y-%m-%d %H:%M:%S")
+                created =time
+                con = models.BusinessModel(uid = uid,name = name, category = category, 
+                                           subcategory = subcategory,country = country, 
+                            contact_person = contact_person,contact_phone = contact_phone,
+                            email = email,password = password, address = address,
+                            correspondance_address = correspondance_address,
+                            alias_name = alias_name,legal_structure = legal_structure,
+                            company_website = company_website, joined = created,
+                            registration_no = registration_no)
+                con.save()
+                return JsonResponse({'error':'false', 'msg':'Business Data created'})  
+            else:
+                return JsonResponse({'error':'true','msg':'Business creation failed','form':form.errors})        
+            
+            
+        
